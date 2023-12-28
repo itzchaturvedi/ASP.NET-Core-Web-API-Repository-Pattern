2219f322d03c76a4861148da9e9bf8967829d9ce
Priyanshu Kuldeep <itzchaturvedi@gmail.com>
2023-12-28 17:41:19 +0530

    Adding DB connection

--- Repository Pattern/Program.cs
+++ Repository Pattern/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Repository_Pattern.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Database Connection Service
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
